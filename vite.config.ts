6be02d5869f683665aad5ea3f784f4db880436d3
Olexander Dobrelya <olexanderdobrelya23@gmail.com>
2026-06-04 12:12:55 +0300

    Set up GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Required for GitHub Pages: sets the base path to the repository name
+  base: '/LightUA/',
 });
